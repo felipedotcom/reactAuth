04f3632069cb9152e0daa895374a0eacb2abce87
ikyrie <mah.c5r@gmail.com>
2021-01-05 17:13:52 -0300

    Estilizando o formulário e montando a função de envio de posts para o back

---
+++ src/api/posts.js
@@ -0,0 +1,5 @@
+const url = '/posts'
+
+export cria = async (dados) => {
+    
+}
--- src/paginas/CriaPost.jsx
+++ src/paginas/CriaPost.jsx
@@ -1,107 +1,113 @@
 import React from "react";
 import { Formik, Field, Form } from "formik";
+import '../assets/css/componentes/inputs.css'
+
 const CriaPost = () => {
   return (
-    <section className="cartao">
-      <h2 className="cartao__titulo">Complete seu cadastro</h2>
-      <Formik
-        initialValues={{
-          //Puxa pelo campo name do Field
-          title: "Meu post",
-          body: "",
-          metadescription: "",
-          categoria: "bem-estar",
-          subcategoria: "saude",
-        }}
-        onSubmit={(infos) => {
-          console.log(infos);
-        }}
-      >
-        <Form className="formulario flex flex--coluna">
-          <fieldset>
-            <legend className="formulario__legenda">Informações do post</legend>
-            <div className="input-container">
-              <Field
-                name="title"
-                id="title"
-                className="input"
-                type="text"
-                placeholder="Título"
-                data-tipo="title"
-                required
-              />
-              <label className="input-label" htmlFor="title">
-                Título
+    <main className="container flex flex--centro">
+      <section className="cartao">
+        <h2 className="cartao__titulo">Complete seu cadastro</h2>
+        <Formik
+          initialValues={{
+            //Puxa pelo campo name do Field
+            title: "Meu post",
+            body: "",
+            metadescription: "",
+            categoria: "",
+            subcategoria: "",
+          }}
+          onSubmit={(infos) => {
+            console.log(infos);
+          }}
+        >
+          <Form className="formulario flex flex--coluna">
+            <fieldset>
+              <legend className="formulario__legenda">Informações do post</legend>
+              <div className="input-container">
+                <Field
+                  name="title"
+                  id="title"
+                  className="input"
+                  type="text"
+                  placeholder="Título"
+                  data-tipo="title"
+                  required
+                />
+                <label className="input-label" htmlFor="title">
+                  Título
               </label>
-            </div>
-            <div className="input-container">
-              <Field
-                name="metadescription"
-                id="metadescription"
-                className="input"
-                type="text"
-                placeholder="Descrição do post"
-                required
-              />
-              <label className="input-label" htmlFor="metadescription">
-                Descrição do post
+              </div>
+              <div className="input-container">
+                <Field
+                  name="metadescription"
+                  id="metadescription"
+                  className="input"
+                  type="text"
+                  placeholder="Descrição do post"
+                  required
+                />
+                <label className="input-label" htmlFor="metadescription">
+                  Descrição do post
               </label>
-            </div>
-            <div className="input-container">
-              <Field
-                name="body"
-                id="body"
-                className="input"
-                type="text"
-                placeholder="Conteúdo"
-                required
-              />
-              <label className="input-label" htmlFor="body">
-                Conteúdo
+              </div>
+              <div className="input-container">
+                <Field
+                  name="body"
+                  id="body"
+                  className="input"
+                  type="text"
+                  placeholder="Conteúdo"
+                  required
+                />
+                <label className="input-label" htmlFor="body">
+                  Conteúdo
               </label>
-            </div>
+              </div>
 
-            <div className="input-container">
-              <Field
-                name="categoria"
-                id="categoria"
-                as="select"
-                className="input"
-                placeholder="Conteúdo"
-                required
-              >
-                <option value="bem-estar">Bem-estar</option>
-                <option value="comportamento">Comportamento</option>
-              </Field>
-              <label className="input-label" htmlFor="body">
-                Categorias
+              <div className="input-container">
+                <Field
+                  name="categoria"
+                  id="categoria"
+                  as="select"
+                  className="input"
+                  placeholder="Conteúdo"
+                  required
+                >
+                  <option value="" disabled>Selecione</option>
+                  <option value="bem-estar">Bem-estar</option>
+                  <option value="comportamento">Comportamento</option>
+                </Field>
+                <label className="input-label" htmlFor="body">
+                  Categorias
               </label>
-            </div>
+              </div>
 
-            <div className="input-container">
-              <Field
-                name="subcategoria"
-                id="subcategoria"
-                as="select"
-                className="input"
-                placeholder="Conteúdo"
-                required
-              >
-                <option value="saude">Saúde</option>
-                <option value="higiene">Higiene</option>
-                <option value="treinamento">Treinamento</option>
-                <option value="educacao">Educação</option>
-              </Field>
-              <label className="input-label" htmlFor="body">
-                SubCategoria
+              <div className="input-container">
+                <Field
+                  name="subcategoria"
+                  id="subcategoria"
+                  as="select"
+                  className="input"
+                  placeholder="Conteúdo"
+                  required
+                >
+                  <option value="" disabled>Selecione</option>
+                  <option value="saude" defaultValue>Saúde</option>
+                  <option value="higiene">Higiene</option>
+                  <option value="treinamento">Treinamento</option>
+                  <option value="educacao">Educação</option>
+                </Field>
+                <label className="input-label" htmlFor="body">
+                  SubCategoria
               </label>
-            </div>
-          </fieldset>
+              </div>
+            </fieldset>
 
-          <button type="submit" className="botao">Criar</button>
-        </Form>
-      </Formik>
-    </section>
+            <button type="submit" className="botao">Criar</button>
+          </Form>
+        </Formik>
+      </section>
+    </main>
   );
 };
 
