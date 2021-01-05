1f21f6508844f3ac533a313e51089201727dc7af
Bugan <dones.bugan@gmail.com>
2021-01-05 16:26:27 -0300

    add Formik

--- src/paginas/CriaPost.jsx
+++ src/paginas/CriaPost.jsx
@@ -1,35 +1,108 @@
-import React from 'react'
-
+import React from "react";
+import { Formik, Field, Form } from "formik";
 const CriaPost = () => {
-    return (
-        <section className="cartao">
-            <h2 className="cartao__titulo">Complete seu cadastro</h2>
-            <form action="./cadastro_concluido.html" className="formulario flex flex--coluna">
-                <fieldset>
-                    <legend className="formulario__legenda">Informações do post</legend>
-                    <div className="input-container">
-                        <input name="title" id="title" className="input" type="text" placeholder="Título" data-tipo="title" required/>
-                        <label className="input-label" for="title">Título</label>
-                    </div>
-                    <div className="input-container">
-                        <input name="metadescription" id="metadescription" className="input" type="text" placeholder="Descrição do post" required/>
-                        <label className="input-label" for="metadescription">Descrição do post</label>
-                    </div>
-                    <div className="input-container">
-                        <input name="body" id="body" className="input" type="text" placeholder="Conteúdo" required/>
-                        <label className="input-label" for="body">Conteúdo</label>
-                    </div>
+  return (
+    <section className="cartao">
+      <h2 className="cartao__titulo">Complete seu cadastro</h2>
+      <Formik
+        initialValues={{
+          //Puxa pelo campo name do Field
+          title: "Meu post",
+          body: "",
+          metadescription: "",
+          categoria: "",
+          subcategoria: "",
+        }}
+        onSubmit={(infos) => {
+          console.log(infos);
+        }}
+      >
+        <Form className="formulario flex flex--coluna">
+          <fieldset>
+            <legend className="formulario__legenda">Informações do post</legend>
+            <div className="input-container">
+              <Field
+                name="title"
+                id="title"
+                className="input"
+                type="text"
+                placeholder="Título"
+                data-tipo="title"
+                required
+              />
+              <label className="input-label" htmlFor="title">
+                Título
+              </label>
+            </div>
+            <div className="input-container">
+              <Field
+                name="metadescription"
+                id="metadescription"
+                className="input"
+                type="text"
+                placeholder="Descrição do post"
+                required
+              />
+              <label className="input-label" htmlFor="metadescription">
+                Descrição do post
+              </label>
+            </div>
+            <div className="input-container">
+              <Field
+                name="body"
+                id="body"
+                className="input"
+                type="text"
+                placeholder="Conteúdo"
+                required
+              />
+              <label className="input-label" htmlFor="body">
+                Conteúdo
+              </label>
+            </div>
 
-                    <div className="input-container">
-                        <input name="body" id="body" className="input" type="text" placeholder="Conteúdo" required/>
-                        <label className="input-label" for="body">Conteúdo</label>
-                    </div>
-                </fieldset>
-                
-                <button className="botao">Criar</button>
-            </form>
-        </section>
-    )
-}
+            <div className="input-container">
+              <Field
+                name="categoria"
+                id="categoria"
+                as="select"
+                className="input"
+                placeholder="Conteúdo"
+                required
+              >
+                <option value="teste">Teste</option>
+                <option value="teste">Teste</option>
+                <option value="teste">Teste</option>
+              </Field>
+              <label className="input-label" htmlFor="body">
+                Categorias
+              </label>
+            </div>
 
-export default CriaPost
+            <div className="input-container">
+              <Field
+                name="subcategoria"
+                id="subcategoria"
+                as="select"
+                className="input"
+                placeholder="Conteúdo"
+                required
+              >
+                <option value="teste">Teste</option>
+                <option value="teste">Teste</option>
+                <option value="teste">Teste</option>
+              </Field>
+              <label className="input-label" htmlFor="body">
+                SubCategoria
+              </label>
+            </div>
+          </fieldset>
+
+          <button className="botao">Criar</button>
+        </Form>
+      </Formik>
+    </section>
+  );
+};
+
+export default CriaPost;
