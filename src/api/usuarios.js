a73730b22d52b9c284342f56a7c2e3a24ac9e4d9
Bugan <dones.bugan@gmail.com>
2021-01-06 11:34:36 -0300

    salvando token JWT

--- src/api/usuarios.js
+++ src/api/usuarios.js
@@ -1,11 +1,12 @@
-import { api } from './api';
+import { api } from "./api";
 
-const baseUrl = '/usuario';
+const baseUrl = "/usuario";
 
 export const fazerLogin = async (dados) => {
-  const resposta = await api.post(baseUrl + '/login', dados);
+  const resposta = await api.post(baseUrl + "/login", dados);
 
   if (resposta.status === 200) {
+    localStorage.setItem("jwt", resposta.headers.authorization);
     return resposta;
   }
 
