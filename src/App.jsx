a07d951e5bc9eb7b61cc7d37547ac5a6b24ece71
ikyrie <mah.c5r@gmail.com>
2021-01-07 11:12:29 -0300

    Extraindo as rotas do app para um componente de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import Cabecalho from "./components/Cabecalho";
-import Home from "./paginas/Home";
-import Sobre from "./paginas/Sobre";
-import Categoria from "./paginas/Categoria";
-import Post from "./paginas/Post";
-import Pagina404 from "./paginas/Pagina404";
-import CriaPost from "./paginas/CriaPost";
-import Login from './paginas/Login'
-import PrivateRoute from "./components/PrivateRoute";
+import Rotas from "./components/Rotas"
+
 
 
 import "./assets/css/base/base.css";
@@ -18,35 +12,7 @@ function App() {
   return (
     <Router>
       <Cabecalho />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-
-        <Route path="/sobre">
-          <Sobre />
-        </Route>
-
-        <Route path="/categoria/:id">
-          <Categoria />
-        </Route>
-
-        <PrivateRoute path="/posts/cria">
-          <CriaPost />
-        </PrivateRoute>
-
-        <Route path="/login">
-          <Login />
-        </Route>
-
-        <Route path="/posts/:id">
-          <Post />
-        </Route>
-
-        <Route>
-          <Pagina404 />
-        </Route>
-      </Switch>
+      <Rotas />
     </Router>
   );
 }
---
+++ src/components/Rotas.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Home from "../paginas/Home";
+import Sobre from "../paginas/Sobre";
+import Categoria from "../paginas/Categoria";
+import Post from "../paginas/Post";
+import Pagina404 from "../paginas/Pagina404";
+import CriaPost from "../paginas/CriaPost";
+import Login from '../paginas/Login'
+import PrivateRoute from "../components/PrivateRoute";
+
+const Rotas = () => {
+  return (
+    <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/sobre">
+          <Sobre />
+        </Route>
+
+        <Route path="/categoria/:id">
+          <Categoria />
+        </Route>
+
+        <PrivateRoute path="/posts/cria">
+          <CriaPost />
+        </PrivateRoute>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/posts/:id">
+          <Post />
+        </Route>
+
+        <Route>
+          <Pagina404 />
+        </Route>
+      </Switch>
+  );
+};
+
+export default Rotas;
